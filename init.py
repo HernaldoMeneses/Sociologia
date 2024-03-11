e2d88c3e681253e39ecfa56a615729c076ac135f
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2024-03-11 15:52:02 -0300

    Created init.py

---
+++ init.py
@@ -0,0 +1,19 @@
+#Inicio Século XXI
+# mundo intensamente inquietante
+# grandes promessas para o futuro
+# graves conflitos, tensões e divisões sociais, Tecnologia x Natureza
+
+#Como se desenvolveu o mundo atual?
+# Porque tanta coisa muda de geração para a outra
+# Quais o novos rumos?
+# Algumas das principais indagações da sociologia
+#Cultura Intelectual Moderna
+
+# A Sociologia é o estudo da vida social humana, grupos e sociedades.
+# A maioria de nós vê o mundo em termos das características das nossas próprias vidas,
+# com as quais estamos familiarizados.
+# A sociologia abrange a visão mostrando a necessidade de se adoptar pespectivas mais abrangentes
+# do modo como somos e das razões pelas quais agimos.
+
+#18/743
+#file:///L:/Linear_/Sociologia/GIDDENS,%20A.%20Sociologia..pdf
